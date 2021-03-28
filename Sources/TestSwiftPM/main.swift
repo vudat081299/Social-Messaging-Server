7c9a9113051dd34bf0741eafe133ec1187becbd6
Dat vu <vudat81299@gmail.com>
2021-03-28 16:30:56 +0700

    vudat81299

---
+++ Sources/TestSwiftPM/main.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vũ Quý Đạt  on 14/03/2021.
+//
+
+//import Foundation
+
+print("Test SwiftPM.")
+
+if CommandLine.arguments.count != 2 {
+    print("Usage: hello NAME")
+} else {
+    let name = CommandLine.arguments[1]
+    sayHello(name: name)
+    print(CommandLine.arguments)
+}
+// cd to working path and try in terminal $ swift run TestSwiftPM `whoami`
+
+func sayHello(name: String) {
+    print("Hello, \(name)!")
+}
